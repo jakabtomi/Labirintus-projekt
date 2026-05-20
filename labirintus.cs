3e76a7c1b3c7c0c82a501d3d6e9a8dbbf940780d
jakabtomi <113773377+jakabtomi@users.noreply.github.com>
2026-05-20 19:09:11 +0200

    Add files via upload
    meg nagyon eleje

---
+++ labirintus.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabirintusMethods
+{
+    internal class Program
+    {
+        // az elfogadott jarat karakterek
+        static readonly char[] pathChars =
+        {
+            '╬','═',
+            '╦','╩','║',
+            '╣','╠','╗',
+            '╝'
+                ,'╚','╔'
+        };
+
+        /// <summary>
+        /// megadja, hogy hany termet tartalmaz a terkep
+        /// </summary>
+        static int GetRoomNumber(char[,] map)
+        {
+        }
+
+        /// <summary>
+        /// megszamolja a kijaratokat
+        /// </summary>
+        static int GetSuitableEntrance(char[,] map)
+        {
+
+        }
+
+        /// <summary>
+        /// van-e szabálytalan karakter?
+        /// </summary>
+        static bool IsInvalidElement(char[,] map)
+        {
+            char[] valamik =
+            {
+                '.', '█',
+                '╬','═','╦','╩','║','╣','╠','╗','╝','╚','╔'
+            };
+
+
+            /// <summary>
+            /// elerhetetlen jaratelemek listája
+            /// </summary>
+            static List<string> GetUnavailableElements(char[,] map)
+            {
+                List<string> asd = new List<string>();
+            }
+
+            /// <summary>
+            /// labirintus generalasa poziciolista alapjan
+            /// </summary>
+            static char[,] GenerateLabyrinth(List<string> positionsList)
+            {
+                int maxRow = 0;
+                int maxCol = 0;
+
+            }
+
+
+
+            static void Main(string[] args)
+            {  //a minta alapjan
+                char[,] testMap =
+              {
+        { '.', '.', '.', '.', '.', '╔', '═', '═', '═', '═', '═', '╗', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+        { '═', '═', '═', '═', '╗', '║', '.', '.', '.', '.', '.', '║', '.', '.', '.', '.', '.', '.', '█', '.', '.', '.', '.', '.' },
+        { '.', '╔', '═', '═', '╬', '╝', '█', '.', '╔', '═', '═', '═', '╣', '.', '.', '.', '╗', '.', '║', '║', '.', '.', '.', '.' },
+        { '.', '║', '.', '.', '║', '.', '.', '.', '║', '.', '.', '.', '╚', '═', '╦', '═', '╝', '╚', '╦', '╝', '.', '.', '.', '.' },
+        { '.', '╚', '╦', '═', '╩', '═', '═', '═', '╣', '.', '.', '.', '.', '.', '║', '.', '.', '.', '║', '.', '.', '.', '.', '.' },
+        { '.', '.', '╚', '═', '═', '.', '.', '.', '║', '.', '.', '.', '.', '.', '╚', '═', '═', '═', '╝', '.', '.', '.', '.', '.' }
+    };
+
+
+
+
+            }
+        }
+    }
+}
